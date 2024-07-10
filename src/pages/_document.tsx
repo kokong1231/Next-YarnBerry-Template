42ed9296d99757024e7af825fc3d2d791c2e2b2e
Ohs <46322275+kokong1231@users.noreply.github.com>
2024-07-10 12:09:43 +0900

    Initial commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="ko">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Next-Yarn-Standard</title>
+        <meta name="description" content="Yarn Next setting" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
